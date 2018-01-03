0e530e17863baaf08bb2a26822e0392f133439c5
Georgiana <georgiana@reanimate.tech>
2018-01-03 17:52:53 +0200

    node app

---
+++ public/js/demo.js
@@ -0,0 +1,118 @@
+// The URL to your endpoint that maps to s3Credentials function
+var credentialsUrl = 'http://ec2-34-243-62-38.eu-west-1.compute.amazonaws.com/s3_credentials';
+
+$('.upload-btn').on('click', function (){
+  $('.progress-bar').text('0%');
+  $('.progress-bar').width('0%');
+});
+
+$('#upload-input').on('change', function(){
+  $('#require-selection').text("");
+});
+
+$('#uploadVideo').submit(function(){
+
+  var files = $('#upload-input').get(0).files;
+  if (files.length > 0) {
+    var formElement = document.getElementById('uploadVideo');
+    // create a FormData object which will be sent as the data payload in the
+    // AJAX request
+    var formData = new FormData(formElement);
+
+    // loop through all the selected files and add them to the formData object
+    for (var i = 0; i < files.length; i++) {
+      var file = files[i];
+      // add the files to formData object for the data payload
+      formData.append('uploads[]', file, file.name);
+      uploadFileToS3(file, formData);
+    }
+
+  } else {
+    $('#require-selection').text("Please choose a video.");
+  }
+  return false;
+});
+
+function uploadFileToS3(file, formData) {
+  //get credentials to be able to post to s3
+  $.ajax({
+    url: "/s3_credentials",
+    type: 'GET',
+    dataType: 'json',
+    data: {
+      filename: file.name,
+      content_type: file.type
+    },
+
+    success: function(s3Data) {
+      var data = new FormData();
+
+      Object.keys(s3Data.params).forEach(function(key, idx) {
+        data.append(key, s3Data.params[key]);
+      });
+      data.append('file', file, file.name);
+
+      var url = s3Data.endpoint_url;
+      console.log(url);
+
+      // post file to s3
+      $.ajax({
+        url: url,
+        type: 'POST',
+        data: data,
+        processData: false,
+        contentType: false,
+        success: function(data){
+          console.log('Upload successful!\n');
+          notifyUpload(formData.get("name"), formData.get("email"), file.name, data.getElementsByTagName("Location")[0].innerHTML);
+        },
+        xhr: function() {
+          // create an XMLHttpRequest
+          var xhr = new XMLHttpRequest();
+
+          // listen to the 'progress' event
+          xhr.upload.addEventListener('progress', function(evt) {
+
+            if (evt.lengthComputable) {
+              // calculate the percentage of upload completed
+              var percentComplete = evt.loaded / evt.total;
+              percentComplete = parseInt(percentComplete * 100);
+
+              // update the Bootstrap progress bar with the new percentage
+              $('.progress-bar').text(percentComplete + '%');
+              $('.progress-bar').width(percentComplete + '%');
+
+              // once the upload reaches 100%, set the progress bar text to done
+              if (percentComplete === 100) {
+                $('.progress-bar').html('Done');
+              }
+            }
+
+          }, false);
+
+          return xhr;
+        }
+      });
+    }
+  });
+}
+
+function notifyUpload(name, email, filename, location) {
+  data = {
+    "name": name,
+    "email": email,
+    "filename": filename, 
+    "location": location
+  };
+  // post file to s3
+  $.ajax({
+    url: "/analyse",
+    type: 'POST',
+    data: JSON.stringify(data),
+    processData: false,
+    contentType: "application/json",
+    success: function(data){
+      console.log('Successful notification!\n' + data);
+    }
+  });
+}
---
+++ server.js
@@ -0,0 +1,92 @@
+var express = require('express');
+var crypto = require('crypto');
+var path = require('path');
+var AWS = require('aws-sdk');
+var bodyParser = require('body-parser');
+
+var s3 = require('./s3');
+
+var s3Config = {
+  accessKey: process.env.S3_ACCESS_KEY,
+  secretKey: process.env.S3_SECRET_KEY,
+  bucket: process.env.S3_BUCKET,
+  region: process.env.S3_REGION
+};
+
+AWS.config.region = process.env.REGION
+
+var sns = new AWS.SNS();
+var ddb = new AWS.DynamoDB();
+
+var ddbTable =  process.env.ANALYSIS_REQUEST_TABLE;
+var snsTopic =  process.env.NEW_ANALYSIS_REQUEST_TOPIC;
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/s3_credentials', function(request, response) {
+  if (request.query.filename) {
+    var filename =
+      crypto.randomBytes(16).toString('hex') +
+      path.extname(request.query.filename);
+    response.json(s3.s3Credentials(s3Config, {filename: filename, contentType: request.query.content_type}));
+  } else {
+    response.status(400).send('filename is required');
+  }
+});
+
+app.post('/analyse', function(request, response) {
+  if (request.body.email && request.body.location) {
+    var item = {
+      'name': {'S': request.body.name},
+      'email': {'S': request.body.email},
+      'filename': {'S': request.body.filename},
+      'location': {'S': request.body.location}
+    };
+
+    ddb.putItem({
+      'TableName': ddbTable,
+      'Item': item,
+      'Expected': { location: { Exists: false } }        
+    }, function(err, data) {
+      console.log("alo");
+      if (err) {
+        var returnStatus = 500;
+
+        if (err.code === 'ConditionalCheckFailedException') {
+          returnStatus = 409;
+        }
+
+        response.status(returnStatus).end();
+          console.log('DDB Error: ' + err);
+        } else {
+          sns.publish({
+            'Message': 'Name: ' + request.body.name + "\r\nEmail: " + request.body.email 
+                + "\r\nFilename: " + request.body.filename 
+                + "\r\nLocation: " + request.body.location,
+                    'Subject': 'New analysis request!!!',
+                    'TopicArn': snsTopic
+                }, function(err, data) {
+                    if (err) {
+                        response.status(500).end();
+                        console.log('SNS Error: ' + err);
+                    } else {
+                        response.status(201).end();
+                    }
+                });            
+            }
+        });
+  } else {
+    response.status(400).send('Name, email and at least one file are required');
+  }
+});
+
+app.set('port', (process.env.PORT || 5000));
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
